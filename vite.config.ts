b98cc76e6edc222da4f9737165701236ece792d1
Francesco <fra.boasso@gmail.com>
2023-10-26 21:30:56 +0200

    🚀  Trying to fix CORS error in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-    cors: {
-      origin: false,
-    },
+    cors: false,
   },
   plugins: [react()],
 });
