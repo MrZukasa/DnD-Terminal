6010c839bc3775f07c2e8f50679ac803898d644f
Francesco <fra.boasso@gmail.com>
2023-10-26 20:51:48 +0200

    🚑  Hopefully fix cors in vercel env

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
         target: "https://muna.ironarachne.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
+        headers: {
+          "Access-Control-Allow-Credentials": "true",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers":
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
       },
     },
   },
